e21083b4eba10c9f9d4a3d88b45412e828a59a9b
dhennox0871 <dhennox@gmail.com>
2024-04-28 14:42:44 +0700

    login fortify

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +17,17 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'dhenny',
+            'email' => 'dhen@gmail.com',
+            'password'=>Hash::make('12345678'),
+            'role'=>'admin',
+        ]);
+
+        User::factory()->create([
+            'name' => 'test',
+            'email' => 'test@gmail.com',
+            'password'=>Hash::make('87654321'),
+            'role'=>'user',
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
+Route::get('/home', function () {
     return view('pages.dashboard', ['type_menu' => 'dashboard']);
 });
 
-Route::get('/login', function () {
-    return view('pages.auth.auth-login');
-});
+
